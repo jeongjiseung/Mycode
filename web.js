0cf9c4c6228bd95776cf2ddc7ef345d5107b7c48
jeongjiseung <faster789123@naver.com>
2020-03-31 15:08:23 +0900

    user1

---
+++ web.js
@@ -0,0 +1,31 @@
+const express=require('express');
+const app=express()
+const port = 8001
+const path = require('path');
+const ejs = require('ejs');
+const saveRouter=require('./router/form.js');
+// const multiparty=require('multiparty');
+// const socket=require('socket.io');
+// const mysql=require('mysql');
+// const fs = require('fs');
+
+app.use(express.json());
+app.use(express.urlencoded());
+app.use('/',saveRouter);
+
+
+
+app.use(express.static(__dirname + '/save')); // 보안성 증가
+app.use(express.static(__dirname + '/public'));
+
+
+
+app.set('views',path.join(__dirname,'views'));
+app.set("view engine","ejs");
+app.get('/',function(req,res){
+    res.sendFile(__dirname+'/main.html');
+});
+
+app.listen(port,()=>{
+    console.log(`checkmate server start! on port ${port} !`);
+});
